f5df4575ab598cb342c27c9d3b9605d047d4ee3f
Georgi Pl. Berkovski <38726327+dukekn@users.noreply.github.com>
2020-12-15 01:11:38 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -4,8 +4,8 @@ require '../vendor/autoload.php';
 use Ps\CommissionTask\Factory\TransactionFactory;
 use Ps\CommissionTask\Main\Broker;
 
+//get transactions array from file
 $transactions = TransactionFactory::getTransactions('input'.DIRECTORY_SEPARATOR.'input.csv');
 
+// get transaction fees via broker
 $calculate_fee = new Broker($transactions);
-
-//print("<pre>".print_r($transactions,true)."</pre>");
